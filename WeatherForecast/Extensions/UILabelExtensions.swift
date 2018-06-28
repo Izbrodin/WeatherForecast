710a455ab0630a47deb9cbf06cf6a98cc42a53db
Mikhail <mikhail.izbrodin@gmail.com>
2018-06-28 16:05:50 +0300

    extensions

---
+++ WeatherForecast/Extensions/UILabelExtensions.swift
@@ -0,0 +1,18 @@
+//
+//  UILabelExtensions.swift
+//  WeatherForecast
+//
+//  Created by Admin on 13.06.2018.
+//  Copyright © 2018 Admin. All rights reserved.
+//
+
+import UIKit
+
+extension UILabel {
+    func setFontSizeFitWidth() {
+        self.minimumScaleFactor = 0.5
+        self.adjustsFontSizeToFitWidth = true
+        self.numberOfLines = 1
+    }
+}
+
---
+++ WeatherForecast/Extensions/UIViewControllerExtensions.swift
@@ -0,0 +1,17 @@
+//  UIViewControllerExtensions.swift
+//  WeatherForecast
+//
+//  Created by Admin on 28.06.2018.
+//  Copyright © 2018 Admin. All rights reserved.
+//
+
+import UIKit
+
+extension UIViewController {
+    func displayErrorAlert(_ errorText: String) {
+        let alert = UIAlertController(title: "Ошибка", message: errorText, preferredStyle: UIAlertControllerStyle.alert)
+        let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alert.addAction(okAction)
+        self.present(alert, animated: true, completion: nil)
+    }
+}
