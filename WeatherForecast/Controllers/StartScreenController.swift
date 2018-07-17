a1223ddd2cb069855b095a12dcfb62b1774af21e
Mikhail <mikhail.izbrodin@gmail.com>
2018-07-17 15:19:36 +0300

    make alert weak in StartScreenController

--- WeatherForecast/Controllers/StartScreenController.swift
+++ WeatherForecast/Controllers/StartScreenController.swift
@@ -22,8 +22,12 @@ class StartScreenController: UIViewController {
         let alertTitle = "Город"
         let alertMessage = "Введите название города"
         let alertTextFieldPlaceholder = "Название города"
-        let alert = UIAlertController(title: alertTitle, message: alertMessage, preferredStyle: UIAlertControllerStyle.alert)
+        
         let defaultCityName = "Penza"
+        
+        weak var alert = UIAlertController(title: alertTitle, message: alertMessage, preferredStyle: UIAlertControllerStyle.alert)
+
+        if let alert = alert {
         
         alert.addTextField() { (textField) -> Void in
             textField.text = defaultCityName
@@ -55,6 +59,7 @@ class StartScreenController: UIViewController {
         alert.addAction(okAction)
         
         self.present(alert, animated: true, completion: nil)
+        }
     }
     
     func selectTabWithIndex(_ tabIndex: Int) {
